4ffafa678ca5ee7ddd087c37da3564f919a55813
KasuniDulanga <85984851+KasuniDulanga@users.noreply.github.com>
2023-01-26 08:42:55 +0530

    added backend changes

--- restaurant-backend/src/main/java/com/app/restaurant/model/Order.java
+++ restaurant-backend/src/main/java/com/app/restaurant/model/Order.java
@@ -1,5 +1,34 @@
 package com.app.restaurant.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="order")
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long order_id;
+
+    @Column(name="table_no" , nullable = false)
+    private int tableNo;
+
+    @Column(name="net_amount",nullable = false)
+    private double totalAmount;
+
+    @Column(name="status")
+    private String status;
+
+    @Column(name="customer_id",nullable = false)
+    private long customerId;
     
 }
